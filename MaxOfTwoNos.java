681dbb10be58704d7caae1430d49ba53f74e4145
Maitrish Mukherjee <maitrish03@gmail.com>
2021-09-13 20:38:46 +0530

    first commit

---
+++ MaxOfTwoNos.java
@@ -0,0 +1,16 @@
+package maxof2nos;
+
+public class MaxOfTwoNos {
+	public static void main(String args[]) {
+		int a=45;
+		int b=30;
+		
+		if(a>b) {
+			System.out.println(a);
+		}else {
+			System.out.println(b);
+		}
+	}
+	
+
+}
---
+++ MaxOfTwoNosTernary.java
@@ -0,0 +1,14 @@
+package maxof2nos;
+
+public class MaxOfTwoNosTernary {
+	public static void main(String args[]) {
+		
+		int x=50;
+		int y=60;
+		int max=0;
+		max=(x>y)?x:y;
+		
+		System.out.println(max);
+	}
+
+}
